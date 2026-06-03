2b8b15c6937d06e1805ac9b6ced8b88012c29fae
HeyImJoella <joelle.iris@gmail.com>
2026-06-03 22:44:09 +0200

    Basis pagina met nav en secties gemaakt

---
+++ components/contact.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Contact() {
+  return (
+    <div className="min-h-screen" id="contact">
+      <p>Contact</p>
+    </div>
+  );
+}
