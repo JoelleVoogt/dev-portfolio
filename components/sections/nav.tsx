8f5eb434c6a886fe0fe7222618b581322786b7b8
HeyImJoella <joelle.iris@gmail.com>
2026-06-04 11:45:05 +0200

    Fixed nav spacing issue

--- components/sections/nav.tsx
+++ components/sections/nav.tsx
@@ -5,10 +5,10 @@ export default function Nav() {
     <nav className="flex justify-between w-full p-4 fixed top-0 left-0 border-b border-b-green-400 bg-background">
       <div>
         <Link className="text-sm text-brand-400 font-mono" href="/">
-          joelle:~$
+          joellevoogt:~$
         </Link>
       </div>
-      <div className="text-sm text-brand-400 font-mono space-x-3 flex items-center">
+      <div className="text-sm text-brand-400 font-mono space-x-2 flex items-center">
         <Link href="#whoami">whoami</Link>
         <Link href="#projects">projects</Link>
         <Link href="#contact">contact</Link>
