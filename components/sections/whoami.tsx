de658c0c6654b3a9b68b921e9571315d1e197a07
HeyImJoella <joelle.iris@gmail.com>
2026-06-04 11:59:34 +0200

    who am i section added

--- components/sections/whoami.tsx
+++ components/sections/whoami.tsx
@@ -3,36 +3,62 @@ import Image from "next/image";
 
 export default function WhoAmI() {
   return (
-    <div className="min-h-screen flex justify-start items-center" id="whoami">
-      <div className="flex flex-col p-4 justify-start items-center gap-8">
-        <Image
-          src="/avatar-mobile.png"
-          width={116}
-          height={116}
-          className="block md:hidden border-2 border-green-400"
-          alt="Photo of Joelle Voogt"
-        />
-        <Image
-          src="/avatar-desktop.png"
-          width={200}
-          height={200}
-          className="hidden md:block border-2 border-green-400"
-          alt="Photo of Joelle Voogt"
-        />
-        <Image
-          src="/joellevoogt-mobile.svg"
-          width={355}
-          height={54}
-          className="block md:hidden"
-          alt="ASCII of Joelle Voogt"
-        />
-        <Image
-          src="/joellevoogt-desktop.svg"
-          width={760}
-          height={132}
-          className="hidden md:block"
-          alt="ASCII of Joelle Voogt"
-        />
+    <div className="min-h-screen flex flex-col justify-center items-center p-4 gap-14">
+      <div className="flex justify-start items-center" id="whoami">
+        <div className="flex flex-col justify-start items-center gap-8">
+          <Image
+            src="/avatar-mobile.png"
+            width={116}
+            height={116}
+            className="block md:hidden border-2 border-green-400"
+            alt="Photo of Joelle Voogt"
+          />
+          <Image
+            src="/avatar-desktop.png"
+            width={200}
+            height={200}
+            className="hidden md:block border-2 border-green-400"
+            alt="Photo of Joelle Voogt"
+          />
+          <Image
+            src="/joellevoogt-mobile.svg"
+            width={355}
+            height={54}
+            className="block md:hidden"
+            alt="ASCII of Joelle Voogt"
+          />
+          <Image
+            src="/joellevoogt-desktop.svg"
+            width={760}
+            height={132}
+            className="hidden md:block"
+            alt="ASCII of Joelle Voogt"
+          />
+        </div>
+      </div>
+      <div
+        className="grid gap-y-1 w-full font-mono text-sm md:text-base"
+        style={{ gridTemplateColumns: "96px 56px 1fr" }}
+      >
+        <span className="text-mist-400 tracking-widest">SYSTEM</span>
+        <span className="text-green-400">[OK]</span>
+        <span className="text-white">Portfolio v8.2 loaded</span>
+
+        <span className="text-mist-400 tracking-widest">ROLE</span>
+        <span className="text-green-400">[OK]</span>
+        <span className="text-white">Front-end Developer</span>
+
+        <span className="text-mist-400 tracking-widest">STACK</span>
+        <span className="text-green-400">[OK]</span>
+        <span className="text-white">HTML, CSS, JavaScript</span>
+
+        <span className="text-mist-400 tracking-widest">CERTIFIED</span>
+        <span className="text-green-400">[OK]</span>
+        <span className="text-white">7 years UX/UI</span>
+
+        <span className="text-mist-400 tracking-widest">STATUS</span>
+        <span className="text-green-400">[OK]</span>
+        <span className="text-orange-400">Ready to deploy</span>
       </div>
     </div>
   );
