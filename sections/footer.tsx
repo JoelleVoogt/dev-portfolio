2676c4bb18f549723657494026f07b3e16557144
HeyImJoella <joelle.iris@gmail.com>
2026-06-04 09:37:32 +0200

    Overridden light/darkmode preferences

---
+++ sections/footer.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <div className="min-h-screen" id="footer">
+      <p>Footer</p>
+    </div>
+  );
+}
