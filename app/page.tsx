ac28e47739a7d07ae7e38c5ebd19621d23109c23
HeyImJoella <joelle.iris@gmail.com>
2026-06-04 11:43:06 +0200

    Added brand colors

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import Nav from "../components/sections/nav";
 import WhoAmI from "@/components/sections/whoami";
 import Projects from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
+import Footer from "@/components/sections/footer";
 
 export default function Home() {
   return (
-    <div className="mt-16">
+    <div className="bg-black box-border">
       <div className="flex flex-col flex-1 items-center justify-center bg-black">
         <Nav />
         <WhoAmI />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
--- components/sections/whoami.tsx
+++ components/sections/whoami.tsx
@@ -3,8 +3,37 @@ import Image from "next/image";
 
 export default function WhoAmI() {
   return (
-    <div className="min-h-screen" id="whoami">
-      <p>Who am i</p>
+    <div className="min-h-screen flex justify-start items-center" id="whoami">
+      <div className="flex flex-col p-4 justify-start items-center gap-8">
+        <Image
+          src="/avatar-mobile.png"
+          width={116}
+          height={116}
+          className="block md:hidden border-2 border-green-400"
+          alt="Photo of Joelle Voogt"
+        />
+        <Image
+          src="/avatar-desktop.png"
+          width={200}
+          height={200}
+          className="hidden md:block border-2 border-green-400"
+          alt="Photo of Joelle Voogt"
+        />
+        <Image
+          src="/joellevoogt-mobile.svg"
+          width={355}
+          height={54}
+          className="block md:hidden"
+          alt="ASCII of Joelle Voogt"
+        />
+        <Image
+          src="/joellevoogt-desktop.svg"
+          width={760}
+          height={132}
+          className="hidden md:block"
+          alt="ASCII of Joelle Voogt"
+        />
+      </div>
     </div>
   );
 }
