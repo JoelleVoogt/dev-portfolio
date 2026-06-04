5731f1becc6f5d0bec876bc8a35570f5af7c5b78
HeyImJoella <joelle.iris@gmail.com>
2026-06-04 15:07:25 +0200

    Added special old screen filter

--- sections/nav.tsx
+++ sections/nav.tsx
@@ -2,16 +2,24 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="flex justify-between w-full p-4 fixed top-0 left-0 border-b border-b-green-600 bg-background">
-      <div>
-        <Link className="text-sm text-green-400 font-mono" href="/">
-          joelle:~$
-        </Link>
-      </div>
-      <div className="text-sm text-green-400 font-mono space-x-3 flex items-center">
-        <Link href="#whoami">whoami</Link>
-        <Link href="#projects">projects</Link>
-        <Link href="#contact">contact</Link>
+    <nav className="w-full fixed top-0 left-0 border-b border-b-green-400 bg-background z-50 drop-shadow-[0_0_10px_rgba(0,255,65,0.30)]">
+      <div className="flex justify-between max-w-5xl mx-auto p-4">
+        <div>
+          <Link className="text-sm text-brand-400 font-mono" href="/">
+            joellevoogt:~$
+          </Link>
+        </div>
+        <div className="text-sm text-brand-600 font-mono space-x-2 md:space-x-8 flex items-center">
+          <Link className="hover:text-brand-400" href="#whoami">
+            whoami
+          </Link>
+          <Link className="hover:text-brand-400" href="#projects">
+            projects
+          </Link>
+          <Link className="hover:text-brand-400" href="#contact">
+            contact
+          </Link>
+        </div>
       </div>
     </nav>
   );
--- sections/whoami.tsx
+++ sections/whoami.tsx
@@ -1,39 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
+import Terminal from "@/components/sections/terminal";
 
 export default function WhoAmI() {
   return (
-    <div className="min-h-screen flex justify-start items-center" id="whoami">
-      <div className="flex flex-col p-4 justify-start items-center gap-8">
-        <Image
-          src="/avatar-mobile.png"
-          width={116}
-          height={116}
-          className="block md:hidden border-2 border-green-400"
-          alt="Photo of Joelle Voogt"
-        />
-        <Image
-          src="/avatar-desktop.png"
-          width={200}
-          height={200}
-          className="hidden md:block border-2 border-green-400"
-          alt="Photo of Joelle Voogt"
-        />
-        <Image
-          src="/joellevoogt-mobile.svg"
-          width={355}
-          height={54}
-          className="block md:hidden"
-          alt="ASCII of Joelle Voogt"
-        />
-        <Image
-          src="/joellevoogt-desktop.svg"
-          width={760}
-          height={132}
-          className="hidden md:block"
-          alt="ASCII of Joelle Voogt"
-        />
-      </div>
+    <div
+      className="min-h-screen max-w-5xl w-full mx-auto flex justify-start items-center"
+      id="whoami"
+    >
+      <Terminal folder="about">
+        <p>Joelle Voogt</p>
+        <p>Front-end Developer with a UX background</p>
+      </Terminal>
     </div>
   );
 }
