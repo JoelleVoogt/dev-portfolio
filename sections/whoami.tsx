aa9cb379a6c601a2bd88543c5b76525967fad602
HeyImJoella <joelle.iris@gmail.com>
2026-06-05 13:18:41 +0200

    Resolved spacing issues in whoami

--- sections/whoami.tsx
+++ sections/whoami.tsx
@@ -12,21 +12,22 @@ export default function WhoAmI() {
     >
       <Terminal folder="about">
         {/* whoami */}
-        <div className="font-mono flex flex-col gap-12 my-6">
-          <Concatenate folderLocation="whoami" />
-
-          <div className="flex flex-col gap-2">
-            <p className="text-2xl text-brand-400 text-glow-brand">
-              Joelle Voogt
-            </p>
-            <p className="text-surface-200">
-              Front-end Developer with a UX background
-            </p>
-            <p className="text-surface-600">// Based in Delft, Netherlands</p>
+        <div className="font-mono flex flex-col gap-18 my-6">
+          <div className="flex flex-col gap-5">
+            <Concatenate folderLocation="whoami" />
+            <div className="flex flex-col gap-2">
+              <p className="text-2xl text-brand-400 text-glow-brand">
+                Joelle Voogt
+              </p>
+              <p className="text-surface-200">
+                Front-end Developer with a UX background
+              </p>
+              <p className="text-surface-600">// Based in Delft, Netherlands</p>
+            </div>
           </div>
 
           {/* skills */}
-          <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-1">
               <Concatenate folderLocation="cat ./skills.txt" />
             </div>
