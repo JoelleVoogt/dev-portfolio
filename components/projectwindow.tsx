127daba26074989b3239cd66750ae78512fd4436
HeyImJoella <joelle.iris@gmail.com>
2026-06-06 12:24:23 +0200

    Fixed text size and space between project window

--- components/projectwindow.tsx
+++ components/projectwindow.tsx
@@ -7,14 +7,14 @@ export default function ProjectWindow({
 }) {
   return (
     <>
-      <div className="font-mono flex flex-row items-center space-x-4 border border-brand-400 p-4 md:px-6 rounded-t-sm">
+      <div className="font-mono flex flex-row items-center space-x-4 border border-brand-400 p-4 md:px-6 rounded-t-sm justify-between">
         <div className="flex flex-row gap-1 md:gap-4">
-          <p className="text-secondary-400 text-xs md:text-base">$</p>
-          <p className="text-xs md:text-base">cat</p>
-          <p className="text-xs md:text-base">{location}</p>
+          <p className="text-secondary-400 text-sm md:text-base">$</p>
+          <p className="text-sm md:text-base">cat</p>
+          <p className="text-sm md:text-base">{location}</p>
         </div>
         <div className="hidden md:block">
-          <p className="text-xs md:text-sm text-surface-600">{year}</p>
+          <p className="text-sm text-surface-500">{year}</p>
         </div>
       </div>
     </>
