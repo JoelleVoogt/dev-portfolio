c7b18f217cabb244c2c1eb20b2bfdb1ec8e81f13
HeyImJoella <joelle.iris@gmail.com>
2026-06-06 10:42:17 +0200

    Fixed spacing issues on mobile

--- components/projectwindow.tsx
+++ components/projectwindow.tsx
@@ -8,13 +8,13 @@ export default function ProjectWindow({
   return (
     <>
       <div className="font-mono flex flex-row items-center space-x-4 border border-brand-400 p-4 md:px-6 rounded-t-sm">
-        <div className="flex flex-row gap-2 md:gap-4">
-          <p className="text-secondary-400">$</p>
-          <p>cat</p>
-          <p>{location}</p>
+        <div className="flex flex-row gap-1 md:gap-4">
+          <p className="text-secondary-400 text-xs md:text-base">$</p>
+          <p className="text-xs md:text-base">cat</p>
+          <p className="text-xs md:text-base">{location}</p>
         </div>
         <div className="hidden md:block">
-          <p className="text-sm text-surface-600">{year}</p>
+          <p className="text-xs md:text-sm text-surface-600">{year}</p>
         </div>
       </div>
     </>
