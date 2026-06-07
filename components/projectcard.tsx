2b031a1e0dca3d8293e90575a8ac9ae19a2eb586
HeyImJoella <joelle.iris@gmail.com>
2026-06-07 12:18:55 +0200

    Made the card responsive projects

--- components/projectcard.tsx
+++ components/projectcard.tsx
@@ -6,10 +6,10 @@ import { type Project } from "@/data/projects";
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className="flex flex-col bg-[#00FF41]/2">
+    <div className="flex flex-col bg-[#00FF41]/6 shadow-brand-card">
       <ProjectWindow location={project.slug} year={project.year} />
 
-      <div className="flex flex-col gap-6 px-4 py-6 md:p-8 border border-brand-400 rounded-b-sm border-t-0">
+      <div className="flex flex-col gap-8 px-4 py-6 md:px-6 md:py-8 border border-brand-400 rounded-b-sm border-t-0">
         <div className="relative hidden md:block w-full h-[520px] border border-brand-700 rounded-t-sm shadow-brand shadow-brand-card overflow-hidden">
           <Image
             src={project.desktopImage}
@@ -28,16 +28,24 @@ export default function ProjectCard({ project }: { project: Project }) {
           />
         </div>
 
-        <div className="flex flex-col gap-12">
-          <div className="flex flex-col gap-6 md:flex-row md:items-start md:justify-between ">
-            <div className="flex flex-col gap-2 min-w-0 grow">
-              <h3 className="font-bold text-base md:text-lg">
-                {project.title}
-              </h3>
-              <p className="text-surface-400">{project.description}</p>
+        <div className="flex flex-col gap-8">
+          {/* Mobile layout */}
+          <div className="flex flex-col gap-6 md:hidden">
+            <TagList tags={project.tags} />
+            <div className="flex flex-col gap-2">
+              <h3 className="font-bold text-base">{project.title}</h3>
+              <p className="text-surface-300">{project.description}</p>
             </div>
-            <div className="order-first md:order-last shrink-0">
+          </div>
+
+          {/* Desktop layout */}
+          <div className="hidden md:flex md:flex-col md:items-start md:gap-2">
+            <div className="flex flex-row items-center place-content-between w-full">
+              <h3 className="font-bold text-lg">{project.title}</h3>
               <TagList tags={project.tags} />
+            </div>
+            <div className="shrink-0">
+              <p className="text-surface-300 w-full">{project.description}</p>
             </div>
           </div>
 
