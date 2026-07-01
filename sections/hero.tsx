920cc9bec00955bfbb46f1308456fa0d8921fb18
HeyImJoella <joelle.iris@gmail.com>
2026-07-01 11:29:43 +0200

    Add react back

--- sections/hero.tsx
+++ sections/hero.tsx
@@ -45,7 +45,7 @@ export default function Hero() {
         <SystemBoot boot="ROL" status="Front-end Developer" delay="0.2s" />
         <SystemBoot
           boot="STACK"
-          status="Next.js, TypeScript, Tailwind"
+          status="Next.js, React,TypeScript, Tailwind"
           delay="0.4s"
         />
         <SystemBoot boot="ACHTERGROND" status="7 jaar UX/UI" delay="0.6s" />
